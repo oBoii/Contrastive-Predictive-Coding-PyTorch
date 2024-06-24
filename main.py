aca99f2a5c8bbd0b82097b059329cc6ec0e5ab1f
Anonymous user <fabianbusiness@outlook.com>
2024-06-24 22:55:51 +0200

    dataloader from torchaudio

--- main.py
+++ main.py
@@ -3,41 +3,40 @@ main for LibriSpeech
 """
 ## Utilities
 from __future__ import print_function
+
 import argparse
-import random
 import time
-import os
-import logging
 from timeit import default_timer as timer
 
 ## Libraries
 import numpy as np
-
 ## Torch
 import torch
-import torch.nn as nn
-from torch.utils import data
-import torch.nn.functional as F
 import torch.optim as optim
+from torch.utils import data
 
 ## Custrom Imports
 from src.logger_v1 import setup_logs
+from src.model.model import CDCK2
+import torchaudio
+from torchaudio.datasets import LIBRISPEECH
 # from src.data_reader.dataset import RawDataset, ReverseRawDataset, RawXXreverseDataset
-from src.training_v1 import train, trainXXreverse, snapshot
-from src.validation_v1 import validation, validationXXreverse
-from src.model.model import CDCK2, CDCK5, CDCK6
+from src.training_v1 import train, snapshot
+from src.validation_v1 import validation
+
 ############ Control Center and Hyperparameter ###############
 run_name = "cdc" + time.strftime("-%Y-%m-%d_%H_%M_%S")
 print(run_name)
+
 
 class ScheduledOptim(object):
     """A simple wrapper class for learning rate scheduling"""
 
     def __init__(self, optimizer, n_warmup_steps):
         self.optimizer = optimizer
-        self.d_model = 128 
+        self.d_model = 128
         self.n_warmup_steps = n_warmup_steps
-        self.n_current_steps = 0 
+        self.n_current_steps = 0
         self.delta = 1
 
     def state_dict(self):
@@ -66,6 +65,7 @@ class ScheduledOptim(object):
             param_group['lr'] = new_lr
         return new_lr
 
+
 def main():
     ## Settings
     parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
@@ -80,11 +80,11 @@ def main():
     parser.add_argument('--epochs', type=int, default=60, metavar='N',
                         help='number of epochs to train')
     parser.add_argument('--n-warmup-steps', type=int, default=50)
-    parser.add_argument('--batch-size', type=int, default=64, 
+    parser.add_argument('--batch-size', type=int, default=64,
                         help='batch size')
-    parser.add_argument('--audio-window', type=int, default=20480, 
+    parser.add_argument('--audio-window', type=int, default=20480,
                         help='window length to sample from each utterance')
-    parser.add_argument('--timestep', type=int, default=12) 
+    parser.add_argument('--timestep', type=int, default=12)
     parser.add_argument('--masked-frames', type=int, default=20)
     parser.add_argument('--no-cuda', action='store_true', default=False,
                         help='disables CUDA training')
@@ -95,25 +95,41 @@ def main():
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
     print('use_cuda is', use_cuda)
-    global_timer = timer() # global timer
-    logger = setup_logs(args.logging_dir, run_name) # setup logs
+    global_timer = timer()  # global timer
+    logger = setup_logs(args.logging_dir, run_name)  # setup logs
     device = torch.device("cuda" if use_cuda else "cpu")
     model = CDCK2(args.timestep, args.batch_size, args.audio_window).to(device)
-    #model = CDCK5(args.timestep, args.batch_size, args.audio_window).to(device)
-    #model = CDCK6(args.timestep, args.batch_size, args.audio_window).to(device)
+    # model = CDCK5(args.timestep, args.batch_size, args.audio_window).to(device)
+    # model = CDCK6(args.timestep, args.batch_size, args.audio_window).to(device)
     ## Loading the dataset
     params = {'num_workers': 0,
               'pin_memory': False} if use_cuda else {}
 
+    # logger.info('===> loading train, validation and eval dataset')
+    # training_set = RawDataset(args.train_raw, args.train_list, args.audio_window)
+    # validation_set = RawDataset(args.validation_raw, args.validation_list, args.audio_window)
+    # train_loader = data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True,
+    #                                **params)  # set shuffle to True
+    # validation_loader = data.DataLoader(validation_set, batch_size=args.batch_size, shuffle=False,
+    #                                     **params)  # set shuffle to False
+
     logger.info('===> loading train, validation and eval dataset')
-    training_set   = RawDataset(args.train_raw, args.train_list, args.audio_window)
-    validation_set = RawDataset(args.validation_raw, args.validation_list, args.audio_window)
-    train_loader = data.DataLoader(training_set, batch_size=args.batch_size, shuffle=True, **params) # set shuffle to True
-    validation_loader = data.DataLoader(validation_set, batch_size=args.batch_size, shuffle=False, **params) # set shuffle to False
-    # nanxin optimizer  
+    training_set = LIBRISPEECH(args.train_raw, url='train-clean-100', download=True)
+    validation_set = LIBRISPEECH(args.validation_raw, url='dev-clean', download=True)
+    train_loader = data.DataLoader(training_set,
+                                   batch_size=args.batch_size,
+                                   shuffle=True,
+                                   **params)
+
+    validation_loader = data.DataLoader(validation_set,
+                                        batch_size=args.batch_size,
+                                        shuffle=False,
+                                        **params)
+
+    # nanxin optimizer
     optimizer = ScheduledOptim(
         optim.Adam(
-            filter(lambda p: p.requires_grad, model.parameters()), 
+            filter(lambda p: p.requires_grad, model.parameters()),
             betas=(0.9, 0.98), eps=1e-09, weight_decay=1e-4, amsgrad=True),
         args.n_warmup_steps)
 
@@ -123,22 +139,22 @@ def main():
     ## Start training
     best_acc = 0
     best_loss = np.inf
-    best_epoch = -1 
+    best_epoch = -1
     for epoch in range(1, args.epochs + 1):
         epoch_timer = timer()
 
         # Train and validate
-        #trainXXreverse(args, model, device, train_loader, optimizer, epoch, args.batch_size)
-        #val_acc, val_loss = validationXXreverse(args, model, device, validation_loader, args.batch_size)
+        # trainXXreverse(args, model, device, train_loader, optimizer, epoch, args.batch_size)
+        # val_acc, val_loss = validationXXreverse(args, model, device, validation_loader, args.batch_size)
         train(args, model, device, train_loader, optimizer, epoch, args.batch_size)
         val_acc, val_loss = validation(args, model, device, validation_loader, args.batch_size)
-        
+
         # Save
-        if val_acc > best_acc: 
+        if val_acc > best_acc:
             best_acc = max(val_acc, best_acc)
             snapshot(args.logging_dir, run_name, {
                 'epoch': epoch + 1,
-                'validation_acc': val_acc, 
+                'validation_acc': val_acc,
                 'state_dict': model.state_dict(),
                 'validation_loss': val_loss,
                 'optimizer': optimizer.state_dict(),
@@ -147,14 +163,15 @@ def main():
         elif epoch - best_epoch > 2:
             optimizer.increase_delta()
             best_epoch = epoch + 1
-        
+
         end_epoch_timer = timer()
         logger.info("#### End epoch {}/{}, elapsed time: {}".format(epoch, args.epochs, end_epoch_timer - epoch_timer))
-    
+
     ## end 
     end_global_timer = timer()
     logger.info("################## Success #########################")
     logger.info("Total elapsed time: %s" % (end_global_timer - global_timer))
+
 
 if __name__ == '__main__':
     main()
--- src/training_v1.py
+++ src/training_v1.py
@@ -6,11 +6,12 @@ import torch.nn.functional as F
 ## Get the same logger from main"
 logger = logging.getLogger("cdc")
 
+
 def trainXXreverse(args, model, device, train_loader, optimizer, epoch, batch_size):
     model.train()
     for batch_idx, [data, data_r] in enumerate(train_loader):
-        data   = data.float().unsqueeze(1).to(device) # add channel dimension
-        data_r = data_r.float().unsqueeze(1).to(device) # add channel dimension
+        data = data.float().unsqueeze(1).to(device)  # add channel dimension
+        data_r = data_r.float().unsqueeze(1).to(device)  # add channel dimension
         optimizer.zero_grad()
         hidden1 = model.init_hidden1(len(data))
         hidden2 = model.init_hidden2(len(data))
@@ -22,54 +23,57 @@ def trainXXreverse(args, model, device, train_loader, optimizer, epoch, batch_si
         if batch_idx % args.log_interval == 0:
             logger.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tlr:{:.5f}\tAccuracy: {:.4f}\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), lr, acc, loss.item()))
+                       100. * batch_idx / len(train_loader), lr, acc, loss.item()))
+
 
 def train_spk(args, cdc_model, spk_model, device, train_loader, optimizer, epoch, batch_size, frame_window):
-    cdc_model.eval() # not training cdc model 
+    cdc_model.eval()  # not training cdc model
     spk_model.train()
     for batch_idx, [data, target] in enumerate(train_loader):
-        data = data.float().unsqueeze(1).to(device) # add channel dimension
+        data = data.float().unsqueeze(1).to(device)  # add channel dimension
         target = target.to(device)
         hidden = cdc_model.init_hidden(len(data))
         output, hidden = cdc_model.predict(data, hidden)
-        data = output.contiguous().view((-1,256))
-        target = target.view((-1,1))
-        shuffle_indexing = torch.randperm(data.shape[0]) # shuffle frames 
-        data = data[shuffle_indexing,:]
-        target = target[shuffle_indexing,:].view((-1,))
+        data = output.contiguous().view((-1, 256))
+        target = target.view((-1, 1))
+        shuffle_indexing = torch.randperm(data.shape[0])  # shuffle frames
+        data = data[shuffle_indexing, :]
+        target = target[shuffle_indexing, :].view((-1,))
         optimizer.zero_grad()
-        output = spk_model.forward(data) 
+        output = spk_model.forward(data)
         loss = F.nll_loss(output, target)
         loss.backward()
         optimizer.step()
         lr = optimizer.update_learning_rate()
-        pred = output.max(1, keepdim=True)[1] # get the index of the max log-probability
-        acc = 1.*pred.eq(target.view_as(pred)).sum().item()/len(data)
-        
+        pred = output.max(1, keepdim=True)[1]  # get the index of the max log-probability
+        acc = 1. * pred.eq(target.view_as(pred)).sum().item() / len(data)
+
         if batch_idx % args.log_interval == 0:
             logger.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tlr:{:.5f}\tAccuracy: {:.4f}\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data) / frame_window, len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), lr, acc, loss.item()))
+                       100. * batch_idx / len(train_loader), lr, acc, loss.item()))
+
 
 def train(args, model, device, train_loader, optimizer, epoch, batch_size):
     model.train()
-    for batch_idx, data in enumerate(train_loader):
-        data = data.float().unsqueeze(1).to(device) # add channel dimension
+    for batch_idx, (waveform, sample_rate, transcript, speaker_id, chapter_id, utterance_id) in enumerate(train_loader):
+        waveform = waveform.float().unsqueeze(1).to(device)  # add channel dimension
         optimizer.zero_grad()
-        hidden = model.init_hidden(len(data), use_gpu=True)
-        acc, loss, hidden = model(data, hidden)
+        hidden = model.init_hidden(len(waveform), use_gpu=True)
+        acc, loss, hidden = model(waveform, hidden)
 
         loss.backward()
         optimizer.step()
         lr = optimizer.update_learning_rate()
         if batch_idx % args.log_interval == 0:
             logger.info('Train Epoch: {} [{}/{} ({:.0f}%)]\tlr:{:.5f}\tAccuracy: {:.4f}\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), lr, acc, loss.item()))
+                epoch, batch_idx * len(waveform), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), lr, acc, loss.item()))
+
 
 def snapshot(dir_path, run_name, state):
     snapshot_file = os.path.join(dir_path,
-                    run_name + '-model_best.pth')
-    
+                                 run_name + '-model_best.pth')
+
     torch.save(state, snapshot_file)
     logger.info("Snapshot saved to {}\n".format(snapshot_file))
